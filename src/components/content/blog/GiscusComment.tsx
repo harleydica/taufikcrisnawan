9922b33df5b9623b4f8339f93a0a4fd28756ea69
harleydica <taufiklah87@gmail.com>
2022-12-22 02:20:57 +0700

    Giscus fix

--- src/components/content/blog/GiscusComment.tsx
+++ src/components/content/blog/GiscusComment.tsx
@@ -15,7 +15,7 @@ export const GiscusComment = memo(() => {
         theme={gcTheme}
         emitMetadata='0'
         inputPosition='top'
-        repo='harleydica/coba.taufikcrisnawan.dev'
+        repo='harleydica/cobacoba'
         repoId='R_kgDOGh4MEw'
         category='General'
         categoryId='DIC_kwDOGh4ME84CPxWe'
