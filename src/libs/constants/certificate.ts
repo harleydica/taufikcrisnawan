21e2871be56eccaba4126e9384ffda8dd908fcb9
harleydica <taufiklah87@gmail.com>
2022-12-23 18:59:49 +0700

    add certificate

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -1,9 +1,9 @@
 type Albums = Array<{ title: string; src: string }>
 
-const IMG_URL = 'https://ik.imagekit.io/taufik/certificate'
+const IMG_URL = 'https://ik.imagekit.io/mlnzyx/certificate'
 
 const ALBUMS: Albums = [
-  //{ title: 'Basic Web Programming - Dicoding', src: `${IMG_URL}/bdpw.png` },
+  { title: 'Basic Web Programming - Dicoding', src: `${IMG_URL}/Ramadhan2022.jpg` }
 ]
 
 export default ALBUMS
