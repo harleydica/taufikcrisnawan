09ae799166e38bbe7945dbef352c54ec351bb652
harleydica <taufiklah87@gmail.com>
2022-12-26 20:40:41 +0700

    test _document.txt

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,8 @@ export default class CustomDocument extends Document {
           <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-JS5DBWC0GB"></script>
+
           {process.env.NODE_ENV === 'production' && (
             <Script
               async
@@ -25,8 +27,13 @@ export default class CustomDocument extends Document {
               // please change to your data website id
               data-website-id='ed4514ff-0629-43ad-bff0-5bba16e9f785'
               // change to your hosted umami app
-              src='https://umami.taufikcrisnawan.dev/umami.js'
-            />
+              src='https://umami.taufikcrisnawan.dev/umami.js' />
+              <Script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)};{'}'}
+                gtag('js', new Date());
+                gtag('config', 'G-JS5DBWC0GB');
+              </Script>
 
           )}
         </Head>
