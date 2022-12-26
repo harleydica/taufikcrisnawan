399c9f8073448ee08a93d22a78f565fa7cbedf56
harleydica <taufiklah87@gmail.com>
2022-12-26 17:55:32 +0700

    fix pageviews

--- src/services/umami/getToken.ts
+++ src/services/umami/getToken.ts
@@ -1,5 +1,7 @@
 import { UMAMI } from './instance'
 
+const USERNAME = process.env.NEXT_PUBLIC_UMAMI_USERNAME
+const PASSWORD = process.env.NEXT_PUBLIC_UMAMI_PASSWORD
 
 /**
  * run only on the server.
@@ -7,7 +9,7 @@ import { UMAMI } from './instance'
  * @returns The token or null
  */
 export const getToken = async () => {
-  const body = { username: 'admin', password: '!Akugila998865' }
+  const body = { username: USERNAME, password: PASSWORD }
   try {
     const response = await UMAMI.post<{ token: string }>('/api/auth/login', body)
     // return null if the status not 200
