26888d8ed21c53d46d8fc926711b3b24e994eacf
harleydica <taufiklah87@gmail.com>
2022-12-27 23:38:35 +0700

    add google ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,9 +25,12 @@ export default class CustomDocument extends Document {
               // please change to your data website id
               data-website-id='ed4514ff-0629-43ad-bff0-5bba16e9f785'
               // change to your hosted umami app
-              src='https://umami.taufikcrisnawan.dev/umami.js'
-            />
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9254295768355301"crossorigin="anonymous"></script>
+              src='https://umami.taufikcrisnawan.dev/umami.js' 
+              />
+          )},
+          {process.env.NODE_ENV === 'production' && (
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9254295768355301"
+          crossOrigin="anonymous"></script>
           )}
         </Head>
         <body>
