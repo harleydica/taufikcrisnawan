0e58df4506b76c725be78605414331b7b4f9d18e
harleydica <taufiklah87@gmail.com>
2022-12-27 23:09:31 +0700

    add google ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default class CustomDocument extends Document {
           <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-JS5DBWC0GB"></script>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9254295768355301"crossorigin="anonymous"></script>
           {process.env.NODE_ENV === 'production' && (
             <Script
               async
