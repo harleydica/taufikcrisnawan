c8558b1c94ad50388acf59c66ccb12e50db0c6a3
harleydica <taufiklah87@gmail.com>
2022-12-30 08:48:16 +0700

    fix _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,15 +29,15 @@ export default class CustomDocument extends Document {
             />
           )}
           {process.env.NODE_ENV === 'production' && (
-            <script
+            <Script
               async
               // please change to u data
               src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9254295768355301"
-              crossOrigin="anonymous">
-            </script>
+              crossOrigin="anonymous"
+            />
           )}
           {process.env.NODE_ENV === 'production' && (
-            <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            <Script id="toogle-tag" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -45,7 +45,7 @@ export default class CustomDocument extends Document {
           )}
         </Head>
         <body>
-          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXX" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKFXT3K" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
