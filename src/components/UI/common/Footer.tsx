fbb2d5291cecbe10be28a686c20fbea639a01d64
Taufik Crisnawan Santosa <106726975+harleydica@users.noreply.github.com>
2022-12-31 07:04:44 +0700

    fix ads footer

--- src/components/UI/common/Footer.tsx
+++ src/components/UI/common/Footer.tsx
@@ -19,14 +19,14 @@ export const Footer: React.FunctionComponent = () => {
   }
 
   return (
-    <Adsense
+    <footer className={twclsx('layout', 'py-4 mt-5', 'border-t', 'border-theme-300 dark:border-theme-700')}>
+      <Adsense
         client="ca-pub-9254295768355301"
         slot="5171394066"
         style={{ display: 'block' }}
         layout="in-article"
         format="fluid"
       />
-    <footer className={twclsx('layout', 'py-4 mt-5', 'border-t', 'border-theme-300 dark:border-theme-700')}>
       <div className='w-full flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3 md:justify-between'>
         <div className='flex flex-col md:flex-row space-y-3 md:space-x-3 md:space-y-0 w-full md:max-w-max'>
           {APP_ROUTE.map((route) => (
