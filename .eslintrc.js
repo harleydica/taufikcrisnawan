b477ee78bcbd38043d714b7c5b5f97ac2390f92a
harleydica <taufiklah87@gmail.com>
2023-01-08 15:39:57 +0700

    update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@
 module.exports = {
   extends: ['plugin:@typescript-eslint/recommended', 'next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     'prefer-const': 'warn',
     'import/no-duplicates': 'error',
     '@typescript-eslint/no-extra-semi': 'off'
