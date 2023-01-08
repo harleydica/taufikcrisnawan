c9de87f1139bb89b1e11db2de355dbdcac2b1fff
Taufik Crisnawan Santosa <106726975+harleydica@users.noreply.github.com>
2023-01-08 17:00:50 +0700

    fix spotify

--- src/libs/spotify.ts
+++ src/libs/spotify.ts
@@ -4,7 +4,6 @@ const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
 const basic = btoa(`${client_id}:${client_secret}`);
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
@@ -14,12 +13,8 @@ const getAccessToken = async () => {
       Authorization: `Basic ${basic}`,
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: new URLSearchParams({
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    })
+    body: `grant_type=refresh_token&refresh_token=${refresh_token}`
   });
-
   return response.json();
 };
 
@@ -27,16 +22,6 @@ export const getNowPlaying = async () => {
   const { access_token } = await getAccessToken();
 
   return fetch(NOW_PLAYING_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${access_token}`
-    }
-  });
-};
-
-export const getTopTracks = async () => {
-  const { access_token } = await getAccessToken();
-
-  return fetch(TOP_TRACKS_ENDPOINT, {
     headers: {
       Authorization: `Bearer ${access_token}`
     }
