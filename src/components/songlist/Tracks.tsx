f116d49d90bfabbb35864d691d8efbdfde115ff0
harleydica <taufiklah87@gmail.com>
2023-01-08 21:46:20 +0700

    repiew css

--- src/components/songlist/Tracks.tsx
+++ src/components/songlist/Tracks.tsx
@@ -1,26 +1,25 @@
-  export default function Track(track: any) {
-    return (
-      <div className="flex flex-row items-baseline border-b border-gray-300 dark:border-gray-700 max-w-3xl w-full mt-8">
-        <p className="text-sm font-bold text-gray-400 dark:text-gray-600">
-          {track.ranking}
+export default function Track(track) {
+  return (
+    <div className="flex flex-row items-baseline border-b border-theme-300 dark:border-theme-700 max-w-3xl w-full mt-8">
+      <p className="text-sm font-bold text-theme-500 dark:text-gray-400">
+        {track.ranking}
+      </p>
+      <div className="flex flex-col pl-3">
+        <a
+          className="font-medium text-gray-900 dark:text-gray-100 truncate w-60 sm:w-96 md:w-full"
+          href={track.songUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {track.title}
+        </a>
+        <p
+          className="text-theme-500 dark:text-gray-400 mb-4 truncate w-60 sm:w-96 md:w-full"
+          color="gray.500"
+        >
+          {track.artist}
         </p>
-        <div className="flex flex-col pl-3">
-          <a
-            className="font-medium text-gray-900 dark:text-gray-100 truncate w-60 sm:w-96 md:w-full"
-            href={track.songUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {track.title}
-          </a>
-          <p
-            className="text-gray-500 mb-4 truncate w-60 sm:w-96 md:w-full"
-            color="gray.500"
-          >
-            {track.artist}
-          </p>
-        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
