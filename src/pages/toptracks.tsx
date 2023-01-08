76bf228b5a8768127c5b40176a1a6b73ab95eebe
harleydica <taufiklah87@gmail.com>
2023-01-08 19:32:27 +0700

    test toptracks

--- src/pages/toptracks.tsx
+++ src/pages/toptracks.tsx
@@ -1,21 +1,22 @@
 import { Hero, LayoutPage } from '@/UI/templates'
 import { generateOgImage, getMetaPage } from '@/libs/metapage'
-import TopTracks from '@/components/songlist/TopTracks';
+import TopTracks from '@/components/songlist/TopTracks'
+import type { NextPage } from 'next'
 
 const meta = getMetaPage({
     title: 'Top Tracks Spotify',
     description:
-        "Curious what I'm currently jamming to? Here's my top tracks on Spotify updated daily."
+      "Curious what I'm currently jamming to? Here's my top tracks on Spotify updated daily.",
     keywords: ['toptracks', 'spotify', 'taufikcrisnawan.dev'],
     og_image: generateOgImage({
-        title: 'Top Tracks Spotify',
-        subTitle: "My top tracks on Spotify",
-        theme: 'dark'
+      title: 'Top Tracks Spotify',
+      subTitle: "My top tracks on Spotify",
+      theme: 'dark'
     }),
     og_image_alt: 'Top Tracks Spotify — taufikcrisnawan.dev',
     slug: '/toptracks',
     type: 'website'
-})
+  })
 
 const ToptracksPage: NextPage = () => {
 
