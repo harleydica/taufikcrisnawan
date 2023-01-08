67ff1bee6fcb31774850e8c61b1a33e1efda3e6c
harleydica <taufiklah87@gmail.com>
2023-01-08 22:38:09 +0700

    fix hover

--- src/components/songlist/Tracks.tsx
+++ src/components/songlist/Tracks.tsx
@@ -1,5 +1,3 @@
-import { UnstyledLink } from '@/UI/links'
-
 export default function Track(track: any) {
   return (
     <div className="pb-4 flex flex-row items-baseline border-b border-theme-300 dark:border-theme-700 max-w-3xl w-full mt-8">
@@ -7,15 +5,14 @@ export default function Track(track: any) {
         {track.ranking}
       </p>
       <div className="flex flex-col pl-3">
-        <h5>
-          <UnstyledLink
-            className="border-b border-dashed border-transparent hover:border-theme-500 dark:hover:border-theme-300"
-            href={track.songUrl}
-            target="_blank"
-          >
-            {track.title}
-          </UnstyledLink>
-        </h5>
+        <a
+          className="border-b border-dashed border-transparent hover:border-theme-500 dark:hover:border-theme-300 no-underline"
+          href={track.songUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {track.title}
+        </a>
         <p
           className="text-theme-500 dark:text-gray-400 mb-4 truncate w-60 sm:w-96 md:w-full"
           color="gray.500"
