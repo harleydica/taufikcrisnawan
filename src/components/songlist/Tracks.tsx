d116ba1b8e00fa57a5d71c4a96bbb0fd82dcbd36
harleydica <taufiklah87@gmail.com>
2023-01-08 22:59:19 +0700

    fix hover

--- src/components/songlist/Tracks.tsx
+++ src/components/songlist/Tracks.tsx
@@ -6,7 +6,7 @@ export default function Track(track: any) {
       </p>
       <div className="flex flex-col pl-3">
       <a
-          className="font-medium hover:text truncate w-60 sm:w-96 md:w-full"
+          className="font-medium no-underline underline-offset-4 decoration-dashed hover:underline truncate w-60 sm:w-96 md:w-full"
           href={track.songUrl}
           target="_blank"
           rel="noopener noreferrer"
