e033eca08b1dc1825647efebe74e36b621cb936d
harleydica <taufiklah87@gmail.com>
2023-01-10 19:27:49 +0700

    add certificate @dicodingacademy

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -3,7 +3,8 @@ type Albums = Array<{ title: string; src: string }>
 const IMG_URL = 'https://ik.imagekit.io/taufik/certificate'
 
 const ALBUMS: Albums = [
-  { title: 'Belajar Dasar-Dasar DevOps - Dicoding', src: `${IMG_URL}/Belajar_Dasar-Dasar_DevOps.png` }
+  { title: 'Belajar Dasar-Dasar DevOps - Dicoding', src: `${IMG_URL}/Belajar_Dasar-Dasar_DevOps.png` },
+  { title: 'Belajar Jaringan Komputer Untuk Pemula - Dicoding', src: `${IMG_URL}/Belajar_Jaringan_Komputer_Untuk_Pemula.png` }
 ]
 
 export default ALBUMS
