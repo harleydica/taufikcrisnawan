52b155f83093294ded2de9a9ad6e7bf1b201b44a
harleydica <taufiklah87@gmail.com>
2023-01-10 19:03:10 +0700

    add certificate @dicoding

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -3,7 +3,7 @@ type Albums = Array<{ title: string; src: string }>
 const IMG_URL = 'https://ik.imagekit.io/taufik/certificate'
 
 const ALBUMS: Albums = [
-  { title: 'Basic Web Programming - Dicoding', src: `${IMG_URL}/Ramadhan2022.jpg` }
+  { title: 'Belajar Dasar-Dasar DevOps - Dicoding', src: `${IMG_URL}/Belajar_Dasar-Dasar_DevOps.png` }
 ]
 
 export default ALBUMS
