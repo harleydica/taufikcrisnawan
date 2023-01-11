29050ad1fc88147126dc76ec28a66c61f878ca7f
harleydica <taufiklah87@gmail.com>
2023-01-11 18:13:15 +0700

    add certificate from ID Networkes

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -4,7 +4,9 @@ const IMG_URL = 'https://ik.imagekit.io/taufik/certificate'
 
 const ALBUMS: Albums = [
   { title: 'Belajar Dasar-Dasar DevOps - Dicoding', src: `${IMG_URL}/Belajar_Dasar-Dasar_DevOps.png` },
-  { title: 'Belajar Jaringan Komputer Untuk Pemula - Dicoding', src: `${IMG_URL}/Belajar_Jaringan_Komputer_Untuk_Pemula.png` }
+  { title: 'Mikrotik Dasar - ID Networkers', src: `${IMG_URL}/Mikrotik_Dasar_-_ID_Networkers.png` },
+  { title: 'Jaringan Komputer Dasar - ID Networkers', src: `${IMG_URL}/Jaringan_Komputer_Dasar-ID-Networkers.png` },
+  { title: 'Cisco Dasar - ID Networkers', src: `${IMG_URL}/Cisco_Dasar-ID-Networkers.png` }
 ]
 
 export default ALBUMS
