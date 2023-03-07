a2be7b6d67a03495ba76a537cc94cf5814681f23
harleydica <taufiklah87@gmail.com>
2023-03-07 15:26:44 +0700

    add certificate

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -7,7 +7,7 @@ const ALBUMS: Albums = [
   { title: 'Belajar Jaringan Komputer Untuk Pemula - Dicoding', src: `${IMG_URL}/Belajar_Jaringan_Komputer_Untuk_Pemula.png` },
   { title: 'Mikrotik Dasar - ID Networkers', src: `${IMG_URL}/Mikrotik_Dasar_-_ID_Networkers.png` },
   { title: 'Jaringan Komputer Dasar - ID Networkers', src: `${IMG_URL}/Jaringan_Komputer_Dasar-ID-Networkers.png` },
-  { title: 'Cisco Dasar - ID Networkers', src: `${IMG_URL}/Cisco_Dasar-ID-Networkers.png` }
+  { title: 'Cisco Dasar - ID Networkers', src: `${IMG_URL}/Cisco_Dasar-ID-Networkers.png` },
   { title: 'Bootcamp Mikrotik (MTCNA & MTCRE) - ID Networkers', src: `${IMG_URL}/Bootcamp_Mikrotik__MTCNA___MTCRE_.png` }
 ]
 
