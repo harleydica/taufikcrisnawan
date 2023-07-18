60d0341ca5c28d64274a1527b528b3bfedb965f7
Taufik Crisnawan Santosa <106726975+harleydica@users.noreply.github.com>
2023-07-17 19:03:40 -0700

    idn

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -2,7 +2,8 @@ type Albums = Array<{ title: string; src: string }>
 
 const IMG_URL = 'https://ik.imagekit.io/taufik/certificate'
 
-const ALBUMS: Albums = [
+const ALBUMS: Albums = []
+  { title: 'MikroTik Certified Network Associate (MTCNA) - ID Networkers', src: `${IMG_URL}/MTCNA-IDN.png` },
   { title: 'MikroTik Certified Network Associate (MTCNA) - Mikrotik', src: `${IMG_URL}/MTCNA.png` },
   { title: 'Belajar Dasar-Dasar DevOps - Dicoding', src: `${IMG_URL}/Belajar_Dasar-Dasar_DevOps.png` },
   { title: 'Belajar Jaringan Komputer Untuk Pemula - Dicoding', src: `${IMG_URL}/Belajar_Jaringan_Komputer_Untuk_Pemula.png` },
