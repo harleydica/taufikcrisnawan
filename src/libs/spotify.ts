e947b2afa88e56fe73d95000c0e8f19571acb148
Taufik Crisnawan Santosa <106726975+harleydica@users.noreply.github.com>
2023-09-25 17:03:29 +0700

    tes

--- src/libs/spotify.ts
+++ src/libs/spotify.ts
@@ -4,7 +4,7 @@ const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
 
 const basic = btoa(`${client_id}:${client_secret}`)
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
-const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`
+const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10`
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
 
 const getAccessToken = async () => {
