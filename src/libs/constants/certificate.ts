46c12d7d10f460d1ce6666ef10d6b073e2cd86b9
Taufik Crisnawan Santosa <106726975+harleydica@users.noreply.github.com>
2023-10-27 08:52:05 +0700

    new certificate

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -3,9 +3,10 @@ type Albums = Array<{ title: string; src: string }>
 const IMG_URL = 'https://ik.imagekit.io/taufik/certificate'
 
 const ALBUMS: Albums = [
+  { title: 'Cisco Certified Support Technician - Networking (CCST) - Cisco', src: `${IMG_URL}/CCST-Networking.png` },
+  { title: 'MikroTik Certified Network Associate (MTCNA) - Mikrotik', src: `${IMG_URL}/MTCNA.png` },
   { title: 'Juara 1 Lomba Kompetensi Siswa SMK Provinsi D.I. Yogyakarta (ITNSA) - Pusat Prestasi Nasional', src: `${IMG_URL}/LKS-ITNSA-DIY.png` },
   { title: 'MikroTik Certified Network Associate (MTCNA) - ID Networkers', src: `${IMG_URL}/MTCNA-IDN.png` },
-  { title: 'MikroTik Certified Network Associate (MTCNA) - Mikrotik', src: `${IMG_URL}/MTCNA.png` },
   { title: 'Belajar Dasar-Dasar DevOps - Dicoding', src: `${IMG_URL}/Belajar_Dasar-Dasar_DevOps.png` },
   { title: 'Belajar Jaringan Komputer Untuk Pemula - Dicoding', src: `${IMG_URL}/Belajar_Jaringan_Komputer_Untuk_Pemula.png` },
   { title: 'Mikrotik Dasar - ID Networkers', src: `${IMG_URL}/Mikrotik_Dasar_-_ID_Networkers.png` },
