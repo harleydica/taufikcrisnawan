3360d278c98cea0c7f3426b47075062c6007b40c
harleydica <taufiklah87@gmail.com>
2023-10-31 13:46:56 +0700

    add MoE certified

--- src/libs/constants/resume.ts
+++ src/libs/constants/resume.ts
@@ -152,6 +152,11 @@ export const HONOR = [
     href: `${IMG_URL}/Jaringan_Komputer_Dasar-ID-Networkers.png`
   },
   {
+    title: 'Medallion For Excellence Lomba Kompetensi Siswa SMK Nasional (ITNSA)',
+    level: 'Tingkat Nasional 2023 bidang IT Network System Administrator',
+    href: `${IMG_URL}/LKS%20Nasional%20MoE.png`
+  },
+  {
     title: 'Finalis Lomba Kompetensi Siswa SMK Nasional (ITNSA)',
     level: 'Tingkat Nasional 2023 bidang IT Network System Administrator',
     href: `${IMG_URL}/LKS%20Nasional%202023.png`
