3fd242972cd8707b95d6f33fb390ac9624bcc603
Taufik Crisnawan Santosa <106726975+harleydica@users.noreply.github.com>
2025-08-20 15:51:45 +0700

    edit-sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: 'https://taufikcrisnawan.dev',
   generateRobotsTxt: true,
   robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }]
+    policies: [{ userAgent: '*', allow: '/', userAgent: 'Googlebot', allow: '/ads.txt' }]
   },
   sitemapSize: 10000
 }
